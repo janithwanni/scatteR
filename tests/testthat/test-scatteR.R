6fbdcd12f19a679a69e3bf346f1025d95292ec81
Janith Wanniarachchi <janithcwanni@gmail.com>
2021-10-26 16:59:31 +0530

    added tests

--- tests/testthat/test-scatteR.R
+++ tests/testthat/test-scatteR.R
@@ -1,3 +1,6 @@
-test_that("scatteR gives a near good data.frame", {
-  expect_equal(2 * 2, 4)
+test_that("scatteR catches Monotonic", {
+  d <- scatteR(measurements = c("Monotonic" = 1),n_points = 25,init_points = 5)
+  r <- scagnostics::scagnostics(d)
+  print(r)
+  expect_lte(abs(r["Monotonic"]-1.0),0.05)
 })
