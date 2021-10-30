dc9d768aa543958bde3bab6a14c4a985b17299d8
Janith Wanniarachchi <janithcwanni@gmail.com>
2021-10-30 14:51:25 +0530

    modified plot file call

--- experiment/plot_experiment.R
+++ experiment/plot_experiment.R
@@ -1,7 +1,8 @@
 library(tidyverse)
 library(here)
 
-df <- read_csv(here("experiment","experiment_results.csv"))
+df <- read_csv(here("experiment","table.csv")) %>% select(-1)
+# df <- read_csv(here("experiment","experiment_results.csv"))
 
 df %>%
   ggplot(aes(x = measurement_type,y = measurement_value),color="white") +
