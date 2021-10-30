00feffbd0106336784e4b104057bdedf0276d0a2
Janith Wanniarachchi <janithcwanni@gmail.com>
2021-10-30 11:52:01 +0530

    added experiment results and vis

---
+++ experiment/plot_experiment.R
@@ -0,0 +1,10 @@
+library(tidyverse)
+library(here)
+
+df <- read_csv(here("experiment","experiment_results.csv"))
+
+df %>%
+  ggplot(aes(x = measurement_type,y = measurement_value),color="white") +
+  geom_tile(aes(fill = rmse)) +
+  geom_text(aes(label = round(rmse,4)),color = "white",size = 4) +
+  theme_minimal()
