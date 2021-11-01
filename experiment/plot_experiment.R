11fd5b2c06eff38955809e26c036bb61126aec85
Janith Wanniarachchi <janithcwanni@gmail.com>
2021-11-01 14:03:26 +0530

    update experiment plot with labels

--- experiment/plot_experiment.R
+++ experiment/plot_experiment.R
@@ -8,4 +8,6 @@ df %>%
   ggplot(aes(x = measurement_type,y = measurement_value),color="white") +
   geom_tile(aes(fill = rmse)) +
   geom_text(aes(label = round(rmse,4)),color = "white",size = 4) +
-  theme_minimal()
+  theme_minimal() +
+  labs(title = "Experiment results of scatteR",subtitle = "Root Mean Square Error calculated using 20 replicates of 50 points generated using 5 initial points within 3 hours",x = "Scagnostic measurement type",y = "Expected Measurement value",fill = "RMSE") +
+  theme(legend.position = "bottom")
