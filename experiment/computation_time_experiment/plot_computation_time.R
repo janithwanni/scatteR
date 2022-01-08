a350c418b5958450cb872f08bf4c3fe9c8e455f0
Janith Wanniarachchi <janithcwanni@gmail.com>
2022-01-08 18:41:11 +0530

    update plot computation time

--- experiment/computation_time_experiment/plot_computation_time.R
+++ experiment/computation_time_experiment/plot_computation_time.R
@@ -2,12 +2,12 @@ library(tidyverse)
 library(here)
 theme_set(theme_minimal())
 
-d <- read_csv(here("experiment","computation_time_experiment",
+d <- read_csv(here("experiment","computation_time_experiment","results",
                    "scag_type_value_experiment_benchmark.csv")) %>%
   separate(expr,into=c("type","strength"),sep="_")
 d
 
-d %>%
+scag_type_value_plot <- d %>%
   mutate(time = time * 1e-9,type = factor(type),
          # strength = paste("measurement strength =",strength)
          ) %>%
@@ -20,5 +20,11 @@ d %>%
   mutate(type = fct_reorder(type,type_mean_time)) %>%
   ggplot(aes(x = strength,y = time)) +
     geom_bar(stat="identity") +
+    geom_label(aes(label = round(time,1)))+
     facet_wrap(~type,nrow = 3,scales = "free_y") +
-  labs(x = "Measurement strength",y = "Mean time in seconds",title = "Mean time in seconds taken to generate 5 replicates")
+  labs(x = "Measurement strength",y = "Mean time in seconds",
+       title = "Mean time in seconds taken to generate 10 replicates",
+       subtitle = "Mean time  based on 10 replicates of 50 points generated with 5 initial points")
+ggsave(here("experiment","computation_time_experiment","results",
+            "scag_type_value_plot.jpg"),plot = scag_type_value_plot,
+       device = "jpg",width = 11,height = 8,units = "in",dpi = 300)
